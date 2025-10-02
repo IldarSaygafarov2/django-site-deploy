cd31f07c4d32e73b681ff26b06ec5b2b2264d75c
IldarSaygafarov2 <sayildar17@gmail.com>
2025-10-02 17:59:49 +0500

    added fixes for gunicorn configuration

--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -3,7 +3,7 @@ import multiprocessing
 # привязка к локальному хосту нашего сервера
 # nginx
 
-bind = 'http://127.0.0.1:5000'
+bind = '127.0.0.1:5000'
 workers = multiprocessing.cpu_count() + 1
 user = 'ildar'
 timeout = 120
